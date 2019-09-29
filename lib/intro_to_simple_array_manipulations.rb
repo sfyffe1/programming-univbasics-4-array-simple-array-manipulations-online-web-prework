72dd062e43231d832ff7837f8cbb6602799cae41
Sean Fyffe <stfyffe@gmail.com>
2019-09-29 19:03:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -30,4 +30,5 @@ def using_insert (list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
 end
 
-def using_uniq 
+def using_uniq(haircuts)
+  
