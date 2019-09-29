72b3da96f96df4697f18604181742f293670dd64
Sean Fyffe <stfyffe@gmail.com>
2019-09-29 19:09:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -42,6 +42,6 @@ def using_delete(instructors, no_offense_steven)
   instructors.delete(no_offense_steven)
 end
 
-def using_delete_at(famous_robots, deleted_robot)
+def using_delete_at(famous_robots, 2)
   famous_robots(deleted_robot, 2)
 end
