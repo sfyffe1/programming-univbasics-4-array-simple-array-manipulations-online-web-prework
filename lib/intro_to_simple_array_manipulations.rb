4106517c4b2799cce32b98523cb55c5935df1c18
Sean Fyffe <stfyffe@gmail.com>
2019-09-29 18:52:25 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -22,5 +22,6 @@ def shift_with_args(ice_cream_brands)
   ice_cream_brands.shift(2)
 end
 
-def using_concat (my_favorite_things,all_my_favs)
-  
+def using_concat (my_favorite_things,more_favs)
+  my_favorite_things.concat (more_favs) = all_my_favs
+end
