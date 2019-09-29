99bfb3e5ce6d6cd279a28d8f7f6ce7750ddd95dc
Sean Fyffe <stfyffe@gmail.com>
2019-09-29 18:32:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,4 +3,5 @@ def using_push(colors_in_the_rainbow, next_color)
 end
 
 def using_unshift (bouroughs_in_nyc, new_neighborhood)
-  bouroughs_in_nyc
+  bouroughs_in_nyc.unshift(new_neighborhood)
+end
